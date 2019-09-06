64e41d7b1525b2769dd740d94d72f5814567d10d
Jackson Gunther <jackson.gunther@yahoo.com>
2019-09-05 21:47:38 -0600

    Refactor

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -9,31 +9,25 @@ import java.util.concurrent.TimeUnit;
 
 public class test1 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\jgunther\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        //install TestNG tomorrow
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS) ;
         driver.get("https://www.podium.com/");
         driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver, 11);
-        WebElement watchDemo = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#site-navigation > div.desktop-menu > div.right-menu > div.demo-header > div > a")));
-        watchDemo.click();
 
-        WebElement firstName = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("FirstName")));
-        firstName.sendKeys("Tristan");
-        WebElement lastName = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("LastName")));
-        lastName.sendKeys("Test");
-        WebElement email = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Email")));
-        email.sendKeys("t@test.com");
-        WebElement company = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("Company")));
-        company.sendKeys("PODIUM");
-        WebElement mobilePhone = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("MobilePhone")));
-        mobilePhone.sendKeys("333225467");
+        driver.findElement(By.cssSelector("#site-navigation > div.desktop-menu > div.right-menu > div.demo-header > div > a")).click();
+        driver.findElement(By.id("FirstName")).sendKeys("Tristan");
+        driver.findElement(By.id("LastName")).sendKeys("Test");
+        driver.findElement(By.id("Email")).sendKeys("t@test.com");
+        driver.findElement(By.id("Company")).sendKeys("PODIUM");
+        driver.findElement(By.id("MobilePhone")).sendKeys("333225467");
         try {
-            Thread.sleep(10000);
+            Thread.sleep(6000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         driver.quit();
-        driver.close();
     }
 }
