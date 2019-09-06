cefa2c4ef6fa2f04e53dc05e8166f220f4490e8d
jgunther <jackson.gunther@yahoo.com>
2019-09-06 07:48:13 -0600

    Added chromedriver

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 
 public class test1 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\jgunther\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS) ;
         driver.get("https://www.podium.com/");
