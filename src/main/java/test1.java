5b49a1e6f0c5c233943cc1e77a613bafb7d3ade4
jgunther <jackson.gunther@yahoo.com>
2019-09-06 16:00:10 -0600

    Added Customers screen

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -2,13 +2,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.*;
+import org.testng.asserts.Assertion;
 
 import java.util.concurrent.TimeUnit;
 
 public class test1 {
     private WebDriver driver;
-    @BeforeClass
+    @BeforeTest
     public void setup(){
         System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
         driver = new ChromeDriver();
@@ -16,9 +21,9 @@ public class test1 {
         driver.get("https://www.podium.com/");
         driver.manage().window().maximize();
     }
-    @AfterClass
+    @AfterTest
     public void closeTest() {
-        driver.close();
+        driver.quit();
     }
 //    @AfterClass
 //    public void teardown() {
@@ -50,6 +55,33 @@ public class test1 {
         driver.findElement(By.cssSelector("#login > form > div:nth-child(2) > input[type=password]")).sendKeys("T3st!ng");
         driver.findElement(By.cssSelector("#login > form > div:nth-child(2) > a")).click();
         driver.findElement(By.cssSelector("#login > form > button")).click();
-        Thread.sleep(10000);
+        Thread.sleep(2000);
+        String data = driver.findElement(By.className("form-submit-error")).getText();
+        System.out.println(data);
+        String actualString = "Invalid phone or password";
+        Assert.assertEquals(data, actualString);
+        Thread.sleep(4000);
+        driver.navigate().to("https://www.podium.com");
     }
+
+    @Test (priority = 3)
+    public void leads() {
+        Actions actions = new Actions(driver);
+        WebElement leads = driver.findElement(By.cssSelector("#menu-item-1309 > a"));
+        actions.moveToElement(leads).perform();
+        leads.click();
+        String leadsUrl = driver.getCurrentUrl();
+        Assert.assertEquals(leadsUrl, "https://www.podium.com/solutions/leads/");
+    }
+
+    @Test (priority = 4)
+    public void customers() {
+        Actions actions = new Actions(driver);
+        WebElement customers = driver.findElement(By.cssSelector("#menu-item-1314 > a"));
+        actions.moveToElement(customers).perform();
+        customers.click();
+        String customersUrl = driver.getCurrentUrl();
+        Assert.assertEquals(customersUrl, "https://www.podium.com/solutions/customers/");
+    }
+
 }
